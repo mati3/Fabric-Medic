fa565ceec77c3b668a585bce2a73ebfefdafe238
mati <mati331@correo.ugr.es>
2019-06-16 09:49:36 +0200

    actualizo prerrequisitos

--- prerrequisitos.sh
+++ prerrequisitos.sh
@@ -1,34 +1,67 @@
 #!/bin/bash
 
-# ubu-16 Prerrequisitos
+# ubuntu-16 Prerrequisitos para Red Hyperledger Fabric.
 
-sudo apt install curl
+sudo apt install curl -y
 
 # install docker and docker-compose
-sudo apt install docker.io
+echo "      #############################################################
+			 Installing docker
+      #############################################################"
+sudo apt-get update
+sudo apt-get install apt-transport-https ca-certificates gnupg-agent software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu xenial stable" | sudo tee /etc/apt/sources.list.d/docker.list
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io -y
 sudo usermod -aG docker $USER
-exec sudo su -l $USER
+#exec sudo su -l $USER
+
+echo "      #############################################################
+			 Installing docker-compose
+      #############################################################"
 sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
 # install binary Go
+echo "      #############################################################
+			 Installing binary Go
+      #############################################################"
 wget https://dl.google.com/go/go1.12.1.linux-amd64.tar.gz
 tar -xvzf go1.12.1.linux-amd64.tar.gz
 export GOPATH=$HOME/go
 export PATH=$PATH:$GOPATH/bin
 
 # nvm
+echo "      #############################################################
+			 Installing nvm
+      #############################################################"
+#sudo apt install build-essential libssl-dev -y
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
-reboot
+export NVM_DIR="${HOME}/.nvm"
+[ -s "${NVM_DIR}/nvm.sh" ] && . "${NVM_DIR}/nvm.sh"
+[ -s "${NVM_DIR}/bash_completion" ] && . "${NVM_DIR}/bash_completion"
 nvm install 8.9
 nvm use 8.9.4
 npm install npm@5.6.0 -g  		
 
 # pyton
+echo "      #############################################################
+			 Installing pyton
+      #############################################################"
 sudo apt-get install python
 
-# clonar ejemplos de fabric
-git clone https://github.com/hyperledger/fabric-samples.git
-
-# binarios
-curl -sSL http://bit.ly/2ysbOFE | bash -s -- 1.4.1 1.4.1 0.4.15
+# look versions intalled
+echo ''
+echo 'Instalacion completada, versiones:'
+echo ''
+echo -n 'Node:           '
+node --version
+echo -n 'npm:            '
+npm --version
+echo -n 'Docker:         '
+docker --version
+echo -n 'Docker Compose: '
+docker-compose --version
+echo -n 'Python:         '
+python -V
